d046e4cb953b65d17928afaee05e5edb84ff3cc6
Amrita537 <47480631+Amrita537@users.noreply.github.com>
2022-04-12 12:19:43 -0400

    Add files via upload

---
+++ Inspector2.js
@@ -0,0 +1,58 @@
+const batchPlay = require("photoshop").action.batchPlay;
+const getLayerColor=()=>{
+  const result = batchPlay(
+  [
+     {
+        _obj: "get",
+        _target: [
+           {
+              _property: "color"
+           },
+           {
+              _ref: "layer",
+              _enum: "ordinal",
+              _value: "targetEnum"
+           },
+        ],
+        _options: {
+           dialogOptions: "dontDisplay"
+        }
+     }
+  ],{
+     synchronousExecution: false,
+     modalBehavior: "fail"
+  });
+  return result[0].color._value;
+}
+
+
+
+// ******************code from ps inspector******************//
+// const batchPlay = require("photoshop").action.batchPlay;
+//
+// const result = await batchPlay(
+// [
+//    {
+//       _obj: "get",
+//       _target: [
+//          {
+//             _property: "color"
+//          },
+//          {
+//             _ref: "layer",
+//             _id: 11
+//          },
+//          {
+//             _ref: "document",
+//             _id: 225
+//          }
+//       ],
+//       _options: {
+//          dialogOptions: "dontDisplay"
+//       }
+//    }
+// ],{
+//    synchronousExecution: false,
+//    modalBehavior: "fail"
+// });
+// const pinned = result[0];
